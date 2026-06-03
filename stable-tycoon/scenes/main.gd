f2167d5fc7abc48c8431c28969740a99dff21364
veronica-foltz <vfoltz1520@gmail.com>
2026-06-03 09:16:35 -1000

    Brush, rest, and train buttons
    Added brush, rest, and train buttons

--- stable-tycoon/scenes/main.gd
+++ stable-tycoon/scenes/main.gd
@@ -8,6 +8,7 @@ func _ready():
 	update_happiness_label()
 	update_energy_label()
 	update_day_label()
+	update_money_label()
 
 func update_hunger_label():
 	hunger_label.text = "Hunger: " + str(hunger)
@@ -67,8 +68,74 @@ func _on_end_day_button_pressed():
 		
 	if energy < 0:
 		energy = 0
+	
+	money += happiness
 
 	update_day_label()
 	update_hunger_label()
 	update_happiness_label()
 	update_energy_label()
+	update_money_label()
+
+
+var money = 100
+@onready var money_label = $MoneyLabel
+
+func update_money_label():
+	money_label.text = "Money: $" + str(money)
+
+
+func _on_brush_button_pressed():
+
+	happiness += 15
+	
+	if happiness > 100:
+		happiness = 100
+		
+	energy -= 5
+	
+	if energy < 0:
+		energy = 0
+		
+	update_happiness_label()
+	update_energy_label()
+
+
+func _on_rest_button_pressed():
+
+	energy += 20
+	
+	if energy > 100:
+		energy = 100
+		
+	hunger -= 5
+	
+	if hunger < 0:
+		hunger = 0
+		
+	update_energy_label()
+	update_hunger_label()
+
+
+
+func _on_train_button_pressed():
+
+	money += 25
+	
+	energy -= 15
+	hunger -= 10
+	happiness += 5
+	
+	if energy < 0:
+		energy = 0
+		
+	if hunger < 0:
+		hunger = 0
+		
+	if happiness > 100:
+		happiness = 100
+		
+	update_money_label()
+	update_energy_label()
+	update_hunger_label()
+	update_happiness_label()
